53d1c0fbc3e373d184626a4490bb8624fac65a76
Abhijit Sarkar <sarkar.abhijit@outlook.com>
2015-02-20 01:21:02 -0600

    Added Arquillan test

--- jax-ws-deploy-descriptor/src/main/java/name/abhijitsarkar/webservices/jaxws/deploydesc/client/CalculatorClient.java
+++ jax-ws-deploy-descriptor/src/main/java/name/abhijitsarkar/webservices/jaxws/deploydesc/client/CalculatorClient.java
@@ -20,76 +20,71 @@ import javax.xml.ws.soap.SOAPBinding;
 import name.abhijitsarkar.webservices.jaxws.handler.SOAPMessageLoggingHandler;
 
 public class CalculatorClient {
-	private static final String ENDPOINT_URL = "http://localhost:8080/jax-ws-deploy-descriptor/CalculatorService";
-	private static final String SERVICE_NAME = "JAXWSProviderService";
-	private static final String PORT_NAME = "Calculator";
-	private static final String NAMESPACE_URI = "http://deploydesc.jaxws.webservices.abhijitsarkar.name/";
-	// The invocation works even if the QName is wrong; wonder how
-	private static final QName serviceName = new QName(
-			"http://abhijitsarkar.name/webservices/jaxws/deploy-desc/",
-			SERVICE_NAME);
-	private static final QName portName = new QName(NAMESPACE_URI, PORT_NAME);
+    private static final String ENDPOINT_URL = "http://localhost:8080/jax-ws-deploy-descriptor/CalculatorService";
+    private static final String SERVICE_NAME = "JAXWSProviderService";
+    private static final String PORT_NAME = "Calculator";
+    private static final String NAMESPACE_URI = "http://deploydesc.jaxws.webservices.abhijitsarkar.name/";
+    // The invocation works even if the QName is wrong; wonder how
+    private static final QName serviceName = new QName(
+	    "http://abhijitsarkar.name/webservices/jaxws/deploy-desc/",
+	    SERVICE_NAME);
+    private static final QName portName = new QName(NAMESPACE_URI, PORT_NAME);
 
-	private static Dispatch<SOAPMessage> dispatch = null;
+    private static Dispatch<SOAPMessage> dispatch = null;
 
-	static {
-		/** Create a service and add at least one port to it. **/
-		Service service = Service.create(serviceName);
-		service.setHandlerResolver(new HandlerResolver() {
+    static {
+	/** Create a service and add at least one port to it. **/
+	Service service = Service.create(serviceName);
+	service.setHandlerResolver(new HandlerResolver() {
 
-			@SuppressWarnings("rawtypes")
-			@Override
-			public List<Handler> getHandlerChain(PortInfo portInfo) {
-				List<Handler> handlerChain = new ArrayList<Handler>();
-				handlerChain.add(new SOAPMessageLoggingHandler());
+	    @SuppressWarnings("rawtypes")
+	    @Override
+	    public List<Handler> getHandlerChain(PortInfo portInfo) {
+		List<Handler> handlerChain = new ArrayList<Handler>();
+		handlerChain.add(new SOAPMessageLoggingHandler());
 
-				return handlerChain;
-			}
-		});
-		service.addPort(portName, SOAPBinding.SOAP11HTTP_BINDING, ENDPOINT_URL);
-		// The mode must be message if the entity type is SOAPMessage
-		dispatch = service.createDispatch(portName, SOAPMessage.class,
-				Service.Mode.MESSAGE);
-	}
+		return handlerChain;
+	    }
+	});
+	service.addPort(portName, SOAPBinding.SOAP11HTTP_BINDING, ENDPOINT_URL);
+	// The mode must be message if the entity type is SOAPMessage
+	dispatch = service.createDispatch(portName, SOAPMessage.class,
+		Service.Mode.MESSAGE);
+    }
 
-	public static void main(String[] args) {
-		int i = 1;
-		int j = 2;
-		int sum = 0;
+    public int add(int augend, int addend) throws Exception {
+	/** Create SOAPMessage request. **/
+	MessageFactory mf = MessageFactory
+		.newInstance(SOAPConstants.SOAP_1_1_PROTOCOL);
+	// Create a message.
+	SOAPMessage request = mf.createMessage();
 
-		/** Create SOAPMessage request. **/
-		try {
-			MessageFactory mf = MessageFactory
-					.newInstance(SOAPConstants.SOAP_1_1_PROTOCOL);
-			// Create a message.
-			SOAPMessage request = mf.createMessage();
+	// Obtain the SOAP body.
+	SOAPBody body = request.getSOAPBody();
 
-			// Obtain the SOAP body.
-			SOAPBody body = request.getSOAPBody();
+	// Construct the message payload.
+	SOAPElement operation = body
+		.addChildElement("add", "ns", NAMESPACE_URI);
+	SOAPElement arg0 = operation.addChildElement("arg0");
+	arg0.addTextNode(Integer.toString(augend));
+	SOAPElement arg1 = operation.addChildElement("arg1");
+	arg1.addTextNode(Integer.toString(addend));
+	request.saveChanges();
 
-			// Construct the message payload.
-			SOAPElement operation = body.addChildElement("add", "ns",
-					NAMESPACE_URI);
-			SOAPElement arg0 = operation.addChildElement("arg0");
-			arg0.addTextNode(Integer.toString(i));
-			SOAPElement arg1 = operation.addChildElement("arg1");
-			arg1.addTextNode(Integer.toString(j));
-			request.saveChanges();
+	/** Invoke the service endpoint. **/
+	SOAPMessage response = dispatch.invoke(request);
 
-			/** Invoke the service endpoint. **/
-			SOAPMessage response = dispatch.invoke(request);
+	/** Process the response. **/
+	body = response.getSOAPBody();
+	@SuppressWarnings("unchecked")
+	Iterator<SOAPElement> it = body.getChildElements(new QName(
+		NAMESPACE_URI, "addResponse"));
 
-			/** Process the response. **/
-			body = response.getSOAPBody();
-			@SuppressWarnings("unchecked")
-			Iterator<SOAPElement> it = body.getChildElements(new QName(
-					NAMESPACE_URI, "addResponse"));
+	return Integer.valueOf(it.next().getTextContent());
+    }
 
-			sum = Integer.valueOf(it.next().getTextContent());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		System.out.println("Sum of " + i + " and " + j + " is " + sum);
-	}
+    public static void main(String[] args) throws Exception {
+	System.out.printf("Sum of 1 and 2 is: %d%n",
+		new CalculatorClient().add(1, 2));
+    }
 }
